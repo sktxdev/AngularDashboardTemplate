3c35e85a4a6e0d0c34b070b80900e14e5072adfe
sktxdev <sktxdev@gmail.com>
2025-12-23 12:05:18 -0600

    Mac fixes, also enables the debugger

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+const url = require('url');
 let mainWindow;
-let iconPath = path.join(__dirname, 'assets/company.ico');
-console.log('Icon Path:', iconPath);
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: iconPath, 
+    // icon: iconPath,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: true,
+      allowRunningInsecureContent: false
     }
   });
 
   // Development
-  mainWindow.loadURL('http://localhost:4200');
-  
+  mainWindow.loadURL('http://localhost:4200/index.html');
+
+  //mainWindow.loadFile("dist/angulardashboard1/browser/index.html");
+
+    // Open the DevTools (optional)
+  mainWindow.webContents.openDevTools();
+
   // Production
   // mainWindow.loadFile(path.join(__dirname, 'dist/angulardashboard1/browser/index.html'));
-  
+
   mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
+// Create a new window when the app is activated (e.g., clicking the Dock icon on macOS)
 app.on('activate', () => {
-  if (mainWindow === null) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
